18867bfe134e9e81cefd3d3917c3899d711d3c7f
Ayush Upneja <upneja@bu.edu>
2020-11-16 13:54:23 -0500

    Added documentation, variables, and components for prototype demo

---
+++ frontend/variables/charts.js
@@ -0,0 +1,190 @@
+// ##############################
+// // // javascript library for creating charts
+// #############################
+var Chartist = require("chartist");
+
+// ##############################
+// // // variables used to create animation on charts
+// #############################
+var delays = 80,
+  durations = 500;
+var delays2 = 80,
+  durations2 = 500;
+
+// ##############################
+// // // Daily Sales
+// #############################
+
+const dailySalesChart = {
+  data: {
+    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    series: [[120, 170, 70, 170, 230, 180, 380]]
+  },
+  options: {
+    lineSmooth: Chartist.Interpolation.cardinal({
+      tension: 0
+    }),
+    low: 0,
+    high: 500, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+    chartPadding: {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    }
+  },
+  // for animation
+  animation: {
+    draw: function(data) {
+      if (data.type === "line" || data.type === "area") {
+        data.element.animate({
+          d: {
+            begin: 600,
+            dur: 700,
+            from: data.path
+              .clone()
+              .scale(1, 0)
+              .translate(0, data.chartRect.height())
+              .stringify(),
+            to: data.path.clone().stringify(),
+            easing: Chartist.Svg.Easing.easeOutQuint
+          }
+        });
+      } else if (data.type === "point") {
+        data.element.animate({
+          opacity: {
+            begin: (data.index + 1) * delays,
+            dur: durations,
+            from: 0,
+            to: 1,
+            easing: "ease"
+          }
+        });
+      }
+    }
+  }
+};
+
+// ##############################
+// // // Email Subscriptions
+// #############################
+
+const emailsSubscriptionChart = {
+  data: {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "Mai",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ],
+    series: [[5420, 4430, 3200, 7800, 5530, 4530, 3260, 4340, 5680, 6100, 7560, 8950]]
+  },
+  options: {
+    axisX: {
+      showGrid: false
+    },
+    low: 0,
+    high: 10000,
+    chartPadding: {
+      top: 0,
+      right: 5,
+      bottom: 0,
+      left: 0
+    }
+  },
+  responsiveOptions: [
+    [
+      "screen and (max-width: 640px)",
+      {
+        seriesBarDistance: 5,
+        axisX: {
+          labelInterpolationFnc: function(value) {
+            return value[0];
+          }
+        }
+      }
+    ]
+  ],
+  animation: {
+    draw: function(data) {
+      if (data.type === "bar") {
+        data.element.animate({
+          opacity: {
+            begin: (data.index + 1) * delays2,
+            dur: durations2,
+            from: 0,
+            to: 1,
+            easing: "ease"
+          }
+        });
+      }
+    }
+  }
+};
+
+// ##############################
+// // // Completed Tasks
+// #############################
+
+const completedTasksChart = {
+  data: {
+    labels: ["12am", "3pm", "6pm", "9pm", "12pm", "3am", "6am", "9am"],
+    series: [[23, 45, 75, 44, 32, 25, 35, 70]]
+  },
+  options: {
+    lineSmooth: Chartist.Interpolation.cardinal({
+      tension: 0
+    }),
+    low: 0,
+    high: 100, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+    chartPadding: {
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0
+    }
+  },
+  animation: {
+    draw: function(data) {
+      if (data.type === "line" || data.type === "area") {
+        data.element.animate({
+          d: {
+            begin: 600,
+            dur: 700,
+            from: data.path
+              .clone()
+              .scale(1, 0)
+              .translate(0, data.chartRect.height())
+              .stringify(),
+            to: data.path.clone().stringify(),
+            easing: Chartist.Svg.Easing.easeOutQuint
+          }
+        });
+      } else if (data.type === "point") {
+        data.element.animate({
+          opacity: {
+            begin: (data.index + 1) * delays,
+            dur: durations,
+            from: 0,
+            to: 1,
+            easing: "ease"
+          }
+        });
+      }
+    }
+  }
+};
+
+module.exports = {
+  dailySalesChart,
+  emailsSubscriptionChart,
+  completedTasksChart
+};
---
+++ frontend/variables/general.js
@@ -0,0 +1,25 @@
+// ##############################
+// // // Tasks for TasksCard - see Dashboard view
+// #############################
+
+var bugs = [
+  'Video upload gets cut off for users in rural Nebraska',
+  "State Farm API endpoint not responding",
+  "ML Model OCR low accuracy during foggy conditions"
+];
+var website = [
+  "License plate lookup buggy for Hawaii drivers",
+  'Real time graph updating'
+];
+var server = [
+  "Lorum Ipsum",
+  "Lorum Ipsum",
+  'Lorum Ipsum'
+];
+
+module.exports = {
+  // these 3 are used to create the tasks lists in TasksCard - Dashboard view
+  bugs,
+  website,
+  server
+};
