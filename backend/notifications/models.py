cbaa91a15cbc7497aabff46383b46cce76337916
Parker Van Roy <parkervr@bu.edu>
2021-02-07 00:22:02 +0000

    Defined Models
    Signed-off-by: Parker Van Roy <parkervr@bu.edu>

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Notification(models.Model):
+    message_text = models.CharField(max_length=255)
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
+    license_plate = models.CharField(max_length=16)
--- backend/reports/models.py
+++ backend/reports/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Report(models.Model):
+    license_plate = models.CharField(max_length=16)
+    speed = models.IntegerField()
+    infraction = models.CharField() # TO BE CHANGED ?
+    confidence = models.DecimalField()
+    timestamp = models.CharField()
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
+    #photograph = models.ImageField(upload_to="report_captures")
+    unit_id = models.IntegerField()
+
+class Statistics(models.Model):
+    total_reports = models.IntegerField()
+    avg_confidence = models.DecimalField()
+    last_updated = models.DateTimeField(auto_now=True)
+
