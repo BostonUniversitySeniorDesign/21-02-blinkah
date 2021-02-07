02855cab83aaf7b78f42de3947252e86c6c5da16
Will Pine <wpine215@gmail.com>
2021-02-07 00:28:37 +0000

    Add models to admin panel

--- backend/notifications/admin.py
+++ backend/notifications/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from blinkah.notifications.models import Notification
 # Register your models here.
+
+admin.site.register(Notification)
+
--- backend/reports/admin.py
+++ backend/reports/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from blinkah.reports.models import Report, Statistics
 # Register your models here.
+
+admin.site.register(Report)
+admin.site.register(Statistics)
