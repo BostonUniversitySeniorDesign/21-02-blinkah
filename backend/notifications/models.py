e1663e47e7f255f73cadc4176ae2f27b76f917f3
Parker Van Roy <ParkerVR@bu.edu>
2021-02-07 00:42:00 +0000

    Updated models with required args

--- backend/notifications/models.py
+++ backend/notifications/models.py
@@ -4,6 +4,6 @@ from django.db import models
 
 class Notification(models.Model):
     message_text = models.CharField(max_length=255)
-    latitude = models.DecimalField()
-    longitude = models.DecimalField()
+    latitude = models.DecimalField(decimal_places=4,max_digits=10)
+    longitude = models.DecimalField(decimal_places=4,max_digits=10)
     license_plate = models.CharField(max_length=16)
--- backend/reports/models.py
+++ backend/reports/models.py
@@ -5,16 +5,16 @@ from django.db import models
 class Report(models.Model):
     license_plate = models.CharField(max_length=16)
     speed = models.IntegerField()
-    infraction = models.CharField() # TO BE CHANGED ?
-    confidence = models.DecimalField()
-    timestamp = models.CharField()
-    latitude = models.DecimalField()
-    longitude = models.DecimalField()
+    infraction = models.CharField(max_length=255) # TO BE CHANGED ?
+    confidence = models.DecimalField(decimal_places=4,max_digits=10)
+    timestamp = models.CharField(max_length=16)
+    latitude = models.DecimalField(decimal_places=4,max_digits=10)
+    longitude = models.DecimalField(decimal_places=4,max_digits=10)
     #photograph = models.ImageField(upload_to="report_captures")
     unit_id = models.IntegerField()
 
 class Statistics(models.Model):
     total_reports = models.IntegerField()
-    avg_confidence = models.DecimalField()
+    avg_confidence = models.DecimalField(decimal_places=4,max_digits=10)
     last_updated = models.DateTimeField(auto_now=True)
 
