70901dddea65470c794ca2fe2579bd9a4362de5b
Parker Van Roy <parkervr@bu.edu>
2021-03-28 17:14:08 -0400

    Added system sound playing and tested on usb speaker

--- Jetson/audio/pyaudio.py
+++ Jetson/audio/pyaudio.py
@@ -1,2 +1,9 @@
-from playsound import playsound
-playsound('bos_sample.mp3')
+import subprocess
+import time
+
+a = subprocess.Popen(['cvlc', 'bos_sample.mp3'])
+time.sleep( 3 )
+b = subprocess.Popen(['cvlc', 'bos_sample.mp3'])
+time.sleep(3)
+a.kill()
+b.kill()
