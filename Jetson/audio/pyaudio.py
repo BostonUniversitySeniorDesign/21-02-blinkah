02c97e38a77fb9a70dde063832db4b328c6c033c
RamB99 <rbukka@bu.edu>
2021-04-07 11:43:51 -0400

    Notifications with Audio; unable to test on windows, but might work on Linux

--- Jetson/audio/pyaudio.py
+++ Jetson/audio/pyaudio.py
@@ -2,7 +2,7 @@ import subprocess
 import time
 
 a = subprocess.Popen(['cvlc', 'bos_sample.mp3'])
-time.sleep( 3 )
+time.sleep(3)
 b = subprocess.Popen(['cvlc', 'bos_sample.mp3'])
 time.sleep(3)
 a.kill()
