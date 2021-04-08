0d6d672439caa50fe5c74904064edfc54725c871
RamB99 <rbukka@bu.edu>
2021-04-08 10:09:05 -0400

    New notification with Ayush car

--- Jetson/qt5/notif_alert.py
+++ Jetson/qt5/notif_alert.py
@@ -11,23 +11,23 @@ def notif_alert():
     url = "http://161.35.50.175:8000/notifications/"
     lp = "License Plate:"
     nt = "Notification Text:"
-    payload = {'license_plate ': 'lp', 'message_text': 'nt'}
+    payload = {'license_plate': 'lp', 'message_text': 'nt'}
 
     # Get request to get JSON data of notification message & license plate
     r = requests.get(url, params=payload)
     p_text = r.text
     p_tfin = json.loads(p_text)
-    lp_msg3 = (p_tfin[4]['message_text'])
-    lp_lp3 = (p_tfin[4]['license_plate'])
-    lp_audio3 = (p_tfin[4]['audio'])
+    lp_msg3 = (p_tfin[5]['message_text'])
+    lp_lp3 = (p_tfin[5]['license_plate'])
+    lp_audio3 = (p_tfin[5]['audio'])
 
-    full_msg = (lp_msg3 + "!\nLicense plate is: " + lp_lp3)
+    full_msg = (lp_msg3)  # "!\nLicense plate is: " + lp_lp3)
     print(full_msg)
 
-    # urllib.request.urlretrieve(lp_audio3, 'notif.mp3')
-    # # r2 = requests.get(lp_audio3, allow_redirects=True)
-    # # open('notif2.mp3', 'wb').write(r2.content)
-    # a = subprocess.Popen(['cvlc', 'notif.mp3'])
+    urllib.request.urlretrieve(lp_audio3, 'notif.mp3')
+    r2 = requests.get(lp_audio3, allow_redirects=True)
+    open('notif2.mp3', 'wb').write(r2.content)
+    a = subprocess.Popen(['cvlc', 'notif.mp3'])
     # time.sleep(3)
     # a.kill()
     return full_msg
